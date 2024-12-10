f78daf8405feb1486d3f06b158eaf73bbdf0ead8
Jeferson Alvarez <128266747+AlvarezYTF@users.noreply.github.com>
2024-12-10 09:38:21 -0500

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from view.view_calculator import CalculatorView
-from model.model_calculator import CalculatorModel
-from controller.controller_calculator import CalculatorController
-
-if __name__ == "__main__":
-    model = CalculatorModel()
-    view = CalculatorView()
-    controller = CalculatorController(model, view)
-    controller.run()
